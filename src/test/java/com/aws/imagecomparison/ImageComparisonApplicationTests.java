d0195a206f84208798f245cd9c086a07e0368280
kesavap91 <kesavap91@gmail.com>
2021-05-23 21:39:16 +0530

    first commit

---
+++ src/test/java/com/aws/imagecomparison/ImageComparisonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aws.imagecomparison;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImageComparisonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
